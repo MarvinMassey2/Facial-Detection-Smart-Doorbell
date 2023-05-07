aa72e165b61b8a9de6c393a6a428b9fffbd885eb
MarvinMassey2 <92770048+MarvinMassey2@users.noreply.github.com>
2023-05-07 11:32:20 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,28 +1,33 @@
 #Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This scrtipt script..
+#Date: 23.10.20
+#for more haarcascade examples visit:
+#https://github.com/opencv/opencv/tree/master/data/haarcascades
 
+# Importing Packages
 import cv2
-from imutils.video.pivideostream import PiVideoStream
-import imutils
-import time
-import numpy as np
+
+#define haarcascade variable
+haar_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+ds_factor=0.6
 
 class VideoCamera(object):
-    def __init__(self, flip = False):
-        self.vs = PiVideoStream().start()
-        self.flip = flip
-        time.sleep(2.0)
+    #Capture Video from camera - compatible with Pi HQ Camera 
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
 
     def __del__(self):
-        self.vs.stop()
-
-    def flip_if_needed(self, frame):
-        if self.flip:
-            return np.flip(frame, 0)
-        return frame
-
+        self.video.release()
+    
     def get_frame(self):
-        frame = self.flip_if_needed(self.vs.read())
-        ret, jpeg = cv2.imencode('.jpg', frame)
+        #Iterating over each frame
+        stream, image = self.video.read()
+        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        obect_rects=haar_cascade.detectMultiScale(gray,1.3,5)
+ 
+        for (x,y,w,h) in obect_rects:
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+            break
+        #OpenCV video capture to JPEG -> to main.py
+        ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
